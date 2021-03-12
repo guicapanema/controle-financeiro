d80e451ad0daab37d642fd4f274ea8430be348d9
Guilherme Capanema de Barros <guilherme@capanema.me>
2021-03-12 07:23:20 -0300

    Add Account model

---
+++ database/factories/AccountFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Account;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AccountFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Account::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'name' => $faker->words(2, true),
+            'type' => $faker->randomElement([
+                'CHECKING',
+                'SAVINGS',
+                'CREDITCARD',
+            ]),
+            'color' => $faker->hexColor(),
+        ];
+    }
+}
